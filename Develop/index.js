49f5d0c3cba1bdfc0d6321e5b2e747468c15f99e
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-01-23 10:33:02 -0500

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,31 @@
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'date',
+        message: 'What is the date of the project?'.
+    }
+    {
+        type: 'input',
+        name: 'process',
+        message: 'What were your methods of completing the project?',
+    }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
