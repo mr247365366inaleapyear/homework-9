3ebd03d3bc0ed4009c91b29513af333df3371278
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-08 11:43:01 -0500

    added function code for index.js

--- Develop/index.js
+++ Develop/index.js
@@ -3,17 +3,14 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 const generate = require('./utils/generateMarkdown')
 const axios = require('axios');
+const generateMarkdown = require("./utils/generateMarkdown");
+const { userInfo } = require("os");
 
 const questions = [
     {
         type: 'input',
         name: 'title',
         message: 'What is your project title?'
-    },
-    {
-        type: 'input',
-        name: 'date',
-        message: 'What is the date of the project?'.
     },
     {
         type: 'input',
@@ -40,12 +37,31 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your README.md file successfully generated!!!!!")
+    })
 }
 
 // function to initialize program
 function init() {
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thanks for your responses! Getting the Github information now");
 
-}
+        console.log("Generating your README next")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+        await writeFileSync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+};
 
 // function call to initialize program
 init();
