0296f340caf79f0a9fb85712fed04d05a68b75bb
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-08 14:41:17 -0500

    functioning readme generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
+const axios = require("axios");
 const fs = require("fs");
-const markdown = require("./utils/generateMarkdown");
+const generate = require("./utils/generateMarkdown");
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-function promptUser() {
-  return inquirer.prompt([    
+//const writeFileAsync = util.promisify(fs.writeFile);
+const questions = [
+//function promptUser() {
+  //return inquirer.prompt([    
     {
       type: "input",
       name: "title",
@@ -62,9 +63,38 @@ function promptUser() {
         message: "Please provide your email address"
       },
 
-  ]).then(answers => {
-      const { name, location, title, description, github, installation, usage, test, repo, badge, license, contribution, email} = answers;
+  ];//)}.then(answers => {
+//      const { name, location, title, description, github, installation, usage, test, repo, badge, license, contribution, email} = answers;
 
-  })
+//  })
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
+
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file created with success!");
+          });
+        });
+
+});
+
+function init() {
+
 }
+
+init();
 
